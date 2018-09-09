578996e3815b5a76d8e5e151863f6745ac4dcf8c
Tony Robertson <tonyr729@gmail.com>
2018-09-09 15:57:13 -0600

    Import and wrap App in Providor from Redux
    Add devTools extention ability to store
    
    Create root reducer file and import for store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+render(app, document.getElementById('root'));
