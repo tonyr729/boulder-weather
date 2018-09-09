6970d6366183e5e3701f5cbc494ee3bc0102dd2a
Tony Robertson <tonyr729@gmail.com>
2018-09-09 16:02:02 -0600

    Import browser router and wrap App within providor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './index.css';
-import App from './components/App/App';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
 import { rootReducer } from './reducers/index';
+import App from './components/App/App';
+import './index.css';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
   && window.__REDUX_DEVTOOLS_EXTENSION__();
@@ -12,7 +15,9 @@ const store = createStore(rootReducer, devTools);
 
 const app = (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
