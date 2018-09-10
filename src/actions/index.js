800f6c7087b36c17742e342fa8ef6a5b66df1615
Tony Robertson <tonyr729@gmail.com>
2018-09-09 23:39:48 -0600

    Add action and reducer to send last five year data to Store

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,10 @@ export const addPreviousYearData = (previousYearData) => ({
   type: 'ADD_PREVIOUS_YEAR_DATA',
   previousYearData
 });
+export const addLastFiveYearsData = (lastFiveYearsData) => { 
+  console.log(lastFiveYearsData)
+  return ({
+    type: 'ADD_LAST_FIVE_YEARS_DATA',
+    lastFiveYearsData
+  });
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { currentYearReducer } from './currentYearReducer';
 import { previousYearReducer } from './previousYearReducer';
+import { lastFiveYearsReducer } from './lastFiveYearsReducer';
 
 export const rootReducer = combineReducers({
   currentYearData: currentYearReducer,
-  previousYearData: previousYearReducer
+  previousYearData: previousYearReducer,
+  lastFiveYearsData: lastFiveYearsReducer
 });
