623d92ae07e1e83115826facf195d5c8132dc8e7
Tony Robertson <tonyr729@gmail.com>
2018-09-09 19:49:42 -0600

    Add functions to calculate averages based on selected timeframe
    Render is hardcoded for current year and needs swap to selected year

--- src/containers/GeneralDisplay/GeneralDisplay.js
+++ src/containers/GeneralDisplay/GeneralDisplay.js
@@ -6,6 +6,13 @@ import { addCurrentYearData } from '../../actions';
 import './GeneralDisplay.css';
 
 class GeneralDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoaded: false,
+      averages: null
+    }
+  }
 
   componentDidMount(){
     this.getCurrentYear()
@@ -13,17 +20,63 @@ class GeneralDisplay extends Component {
 
   getCurrentYear = async () => {
     const currentYear = (new Date()).getFullYear();
-    const url = `http://localhost:3001/api/v1/year/${currentYear}`;
+    const url = `http://localhost:3001/api/v1/year/2017`;
     const response = await fetch(url);
     const currentYearData = await response.json();
+    this.getAverages(currentYearData.year)
     
     this.props.addCurrentYearData(currentYearData.year);
   }
 
+  getAverages = (data) => {
+    const averagesTotal = {
+      jan: this.getMonthAverage(data.filter(el => el.mon === 1)),
+      feb: this.getMonthAverage(data.filter(el => el.mon === 2)),
+      mar: this.getMonthAverage(data.filter(el => el.mon === 3)),
+      apr: this.getMonthAverage(data.filter(el => el.mon === 4)),
+      may: this.getMonthAverage(data.filter(el => el.mon === 5)),
+      jun: this.getMonthAverage(data.filter(el => el.mon === 6)),
+      jul: this.getMonthAverage(data.filter(el => el.mon === 7)),
+      aug: this.getMonthAverage(data.filter(el => el.mon === 8)),
+      sep: this.getMonthAverage(data.filter(el => el.mon === 9)),
+      oct: this.getMonthAverage(data.filter(el => el.mon === 10)),
+      nov: this.getMonthAverage(data.filter(el => el.mon === 11)),
+      dec: this.getMonthAverage(data.filter(el => el.mon === 12)),
+    };
+    
+    if(!this.state.isLoaded) {
+      this.setState({
+        isLoaded: true,
+        averages: averagesTotal
+      });
+    }
+
+    return averagesTotal;
+  }
+
+  getMonthAverage = (monthData) => {
+    const days = monthData.length
+    const averages = monthData.reduce((averages, day) => {
+      averages.tmax += day.tmax;
+      averages.tmin += day.tmin;
+      averages.snow += day.snow;
+
+      return averages;
+    }, {tmax: 0, tmin: 0, snow: 0})
+
+    averages.tmax = Math.round((averages.tmax / days) * 10) / 10;
+    averages.tmin = Math.round((averages.tmin / days) * 10) / 10;
+    averages.snow = Math.round((averages.snow / days) * 10) / 10;
+    
+    return averages
+  }
+
   render() {
     let year;
+    let janHigh;
     if (this.props.currentYearData.length) {
       year = this.props.currentYearData[0].year
+      janHigh = this.state.averages.jan.tmax
     }
     return (
       <div className="GeneralDisplay">
@@ -35,6 +88,7 @@ class GeneralDisplay extends Component {
         <div className="months-display">
         </div>
         <p>Typical weather in {year}.</p>
+        <p>January's average high is {janHigh}</p>
       </div>
     );
   }
