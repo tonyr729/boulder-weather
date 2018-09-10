6527480cbe7155388e58924c42e8ce6952e3e403
Tony Robertson <tonyr729@gmail.com>
2018-09-10 15:48:09 -0600

    Add actions and reducers for AllData and VictoryData

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import { currentYearReducer } from './currentYearReducer';
 import { previousYearReducer } from './previousYearReducer';
 import { lastFiveYearsReducer } from './lastFiveYearsReducer';
+import { allDataReducer } from './allDataReducer';
+import { victoryDataReducer } from './victoryDataReducer';
 
 export const rootReducer = combineReducers({
   currentYearData: currentYearReducer,
   previousYearData: previousYearReducer,
-  lastFiveYearsData: lastFiveYearsReducer
+  lastFiveYearsData: lastFiveYearsReducer,
+  allData: allDataReducer,
+  victoryData: victoryDataReducer
 });
